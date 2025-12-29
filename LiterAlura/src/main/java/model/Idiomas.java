9968b0a5fcb49229aef6e6be5015c5460cca5883
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2025-12-29 18:53:58 -0500

    Enum to categorize the languages that could appear in a book

---
+++ LiterAlura/src/main/java/model/Idiomas.java
@@ -0,0 +1,29 @@
+package model;
+
+public enum Idiomas {
+    english("en"),
+    spanish("es"),
+    french("fr"),
+    portuguese("pt"),
+    italian("it");
+
+    private String lenguage;
+
+    Idiomas(String lenguage) {
+        this.lenguage = lenguage;
+    }
+
+    // Se realiza el Cast del String del API a alguna de las categorías del Enum
+    public static Idiomas fromString(String text) {
+        // Estamos recorriendo con "categoria" todos los valores (Constantes) del Enum
+        for (Idiomas idioma : Idiomas.values()) {
+            // Sí idioma.lenguage es igual a text, retornara el valor (Constante)
+            // que corresponde a esa categoría.
+            if (idioma.lenguage.equalsIgnoreCase(text)) {
+                return idioma;
+            }
+            //De lo contrarió lanzará una excepción:
+            throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+        }
+    }
+}
