6819a02901a06ee0c0f4e22cf335968602caa375
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2025-12-29 19:10:45 -0500

    Implement an interactive console menu that deploy 5 program functionalities

--- LiterAlura/src/main/java/principal/Principal.java
+++ LiterAlura/src/main/java/principal/Principal.java
@@ -7,10 +7,47 @@ import java.util.Scanner;
 public class Principal {
     private Scanner scanner = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-    private static String URL_BASE = "http:///books?search=";
+    private static String URL_BASE = "http:///books?";
 
-    public void prueba(){
-        System.out.println(consumoAPI.obtenerDatos(URL_BASE));
+    public void menu(){
+        var opcion = -1;
+        while (opcion != 0){
+            var menu = """
+                    1 - buscar libro por título
+                    2 - listar libros registrados en la base de datos
+                    3 - listar autores registrados
+                    4 - listar autores vivos en un determinado año
+                    5 - listar libros por idioma
+                    
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    buscarLibroPorTitulo();
+                    break;
+                case 2:
+                    listarLibrosEnBD();
+                    break;
+                case 3:
+                    listarAutoresEnBD();
+                    break;
+                case 4:
+                    listarAutoresPorFecha();
+                    break;
+                case 5:
+                    listarLibrosPorIdiomaEnBD();
+                    break;
+                default:
+                    System.out.println("Opción invalida");
+                    break;
+
+
+            }
+        }
+
     }
 
 
