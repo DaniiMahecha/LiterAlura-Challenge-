152d92c765b249c199bcac902cfd7bcb6f33c490
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2025-12-31 17:21:16 -0500

    Inyect dependencies

--- LiterAlura/src/main/java/com/example/literalura/LiterAluraApplication.java
+++ LiterAlura/src/main/java/com/example/literalura/LiterAluraApplication.java
@@ -1,13 +1,17 @@
 package com.example.literalura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import principal.Principal;
+import com.example.literalura.principal.Principal;
+import com.example.literalura.repository.AutoresRepository;
 
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
 
+    @Autowired
+    private AutoresRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
@@ -15,7 +19,7 @@ public class LiterAluraApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-       Principal principal = new Principal();
+       Principal principal = new Principal(repository);
        principal.menu();
     }
 }
